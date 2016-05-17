4101ddb8e2b3b56d4400de6830e3ae9656d953f8
yutingjin <steven.yuting.jin@gmail.com>
2016-05-17 23:49:48 +0800

    update record list section

--- react-app/p1/app/views/record/mockData.js
+++ react-app/p1/app/views/record/mockData.js
@@ -4,8 +4,9 @@ var items = [
     id:1,
     code: "TBL12021",
     title: '',
+    doctor: '华佗',
     diagnosis: '激素失调，肾阴虚',
-    time: 12345678,
+    time: 1463450449000,
     content: '常规检查',
     prescription: '沙星胶囊，左氧氟注射液，阿司匹林',
     status: '结束'
@@ -13,8 +14,9 @@ var items = [
     id:2,
     code: "TBL12022",
     title: '',
+    doctor: '李时珍',
     diagnosis: '局部毛囊炎',
-    time: 12345678,
+    time: 1463450449000,
     content: '常规检查',
     prescription: '小清创，小包扎，青霉素，沙星胶囊',
     status: '结束'
--- react-app/p1/app/views/record/section.js
+++ react-app/p1/app/views/record/section.js
@@ -7,32 +7,23 @@ import localization from '../../components/localization/localization'
 
 export default class extends Component {
 
-    renderStatus(order: Object) {
-        return (
-            <View style={styles.priceWrapper}>
-                <Text style={styles.price}>{order.status}</Text>
-            </View>
-        )
-    }
-
     render() {
         const order = this.props.order;
         return (
             <TouchableOpacity style={[styles.section, this.props.style]} onPress={this.props.onPress}>
-                <View style={{flex: 1, flexDirection: 'row'}} >
-                    <View style={styles.container}>
-                        <View style={styles.horizontal}>
-                            <Text style={[styles.text, styles.title]}>
-                              {order.code}
-                              <Text style={[styles.text, styles.time, {textAlign: 'right'}]}>
-                                {fecha.format(order.time, localization.TIME_FORMAT)}
-                              </Text>
-                            </Text>
-                        </View>
-                        <View style={styles.horizontal}>
-                            <Text numberOfLines={1} style={styles.address}>诊断：{order.diagnosis}</Text>
-                            <Text numberOfLines={1} style={styles.address}>处方：{order.prescription}</Text>
-                        </View>
+                <View style={styles.container}>
+                    <View style={[styles.horizontal, {flex:1}]}>
+                        <Text style={[styles.text, styles.title]}>
+                          {order.code}
+                        </Text>
+                        <Text style={styles.time}>
+                          {fecha.format(order.time, localization.TIME_FORMAT)}
+                        </Text>
+                    </View>
+                    <View>
+                        <Text numberOfLines={1} style={styles.details}>医生：{order.doctor}</Text>
+                        <Text numberOfLines={1} style={styles.details}>诊断：{order.diagnosis}</Text>
+                        <Text numberOfLines={1} style={styles.details}>处方：{order.prescription}</Text>
                     </View>
                 </View>
             </TouchableOpacity>
@@ -45,7 +36,7 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
         borderRadius: 5,
         flexDirection: "row",
-        height: 60,
+        height: 85,
         paddingTop: 10,
         paddingLeft: 14,
         paddingRight: 14,
@@ -55,34 +46,25 @@ const styles = StyleSheet.create({
     time: {
         color: '#ababab',
         fontSize: 12,
-        paddingLeft: 14
+        paddingLeft: 14,
+        flex: 1,
+        textAlign: 'right',
+        alignItems: 'flex-end',
+        justifyContent: 'space-between'
     },
-    address: {
-        color: '#6f6f6f'
+    details: {
+        color: '#6f6f6f',
+        paddingTop: 3
     },
     text: {
         fontSize: 14,
     },
-    priceWrapper: {
-    },
-    priceQuote: {
-        fontSize: 8,
-        color: '#5fc7f4',
-        textAlign: 'right'
-    },
-    price: {
-        fontSize: 20,
-        color: '#0093f3',
-        textAlign: 'right'
-    },
     horizontal: {
-        //alignItems: 'center',
+        // alignItems: 'center',
+        flexDirection: "row",
     },
     title: {
         paddingRight: 15
-    },
-    orderSectionReturn: {
-        borderStyle: 'solid'
     },
     container: {
         flex: 1
